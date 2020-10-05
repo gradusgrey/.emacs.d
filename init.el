9e9828e8ad3bcca00a797f3c4d684f99377ba0d7
gradusgrey <ychen10.cxy@gmail.com>
2020-10-05 18:05:15 +0800

    latex setup update, cdlatex customizations

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
  '(cdlatex-math-symbol-prefix 64)
  '(custom-safe-themes
 	 (quote
-		("1ed5c8b7478d505a358f578c00b58b430dde379b856fbcb60ed8d345fc95594e" default)))
+		("0ad7f1c71fd0289f7549f0454c9b12005eddf9b76b7ead32a24d9cb1d16cbcbd" "1ed5c8b7478d505a358f578c00b58b430dde379b856fbcb60ed8d345fc95594e" default)))
  '(doc-view-continuous t)
  '(doom-themes-enable-bold t)
  '(indent-guide-global-mode t)
@@ -37,18 +37,16 @@
 		(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
  '(package-selected-packages
 	 (quote
-		(pdf-tools exec-path-from-shell indent-guide
- org-pomodoro elpygen ein markdown-mode magit elpy
- multiple-cursors dimmer highlight-symbol org-journal
- org-capture-pop-frame org-alert pandoc-mode comment-dwim-2
- neotree hl-todo auto-complete doom-modeline doom-themes
- org-bullets yasnippet cdlatex org auctex ## flycheck tide)))
-
+		(pdf-tools exec-path-from-shell indent-guide org-pomodoro elpygen ein markdown-mode magit elpy multiple-cursors dimmer highlight-symbol org-journal org-capture-pop-frame org-alert pandoc-mode comment-dwim-2 neotree hl-todo auto-complete doom-modeline doom-themes org-bullets yasnippet cdlatex org auctex ## flycheck tide)))
  '(python-shell-interpreter "python3.7")
-
  '(tetris-x-colors
-	 [[229 192 123] [97 175 239] [209 154 102] [224 108 117]
-		[152 195 121] [198 120 221] [86 182 194]]))
+	 [[229 192 123]
+		[97 175 239]
+		[209 154 102]
+		[224 108 117]
+		[152 195 121]
+		[198 120 221]
+		[86 182 194]]))
 
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 ;;; .emacs ends here
