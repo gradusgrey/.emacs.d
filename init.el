06b1e1fe3e889db9bb55e69846d442f79e030297
gradusgrey <ychen10.cxy@gmail.com>
2025-07-06 16:38:45 -0400

    use-package-always-ensure

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
 
 ;;; Code:
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(setq use-package-always-ensure t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -25,16 +26,15 @@
  '(doc-view-continuous t)
  '(org-clock-persist 'history)
  '(package-selected-packages
-	 '(## auctex calfw calfw-org cdlatex comment-dwim-2 consult-dir doom-modeline
-							 doom-themes drag-stuff emacsql emojify flycheck
-							 gnu-elpa-keyring-update highlight-symbol hl-todo lean4-mode lsp-mode
-							 magit marginalia markdown-mode mixed-pitch multiple-cursors neotree
-							 nyan-mode org org-alert org-bullets org-capture-pop-frame org-fragtog
-							 org-fragtop org-journal org-mode org-pomodoro org-roam
-							 org-super-agenda pandoc-mode pdf-tools tide use-package yasnippet
-							 zotxt))
- '(package-vc-selected-packages
-	 '((lean4-mode :url "https://github.com/leanprover-community/lean4-mode.git")))
+	 '(## auctex calfw calfw-org cdlatex comment-dwim-2 consult-dir define-word
+				doom-modeline doom-themes drag-stuff emacsql emojify flycheck git-gutter
+				git-gutter-fringe gnu-elpa-keyring-update gptel helm-org-rifle
+				highlight-symbol hl-todo image-roll lean4-mode lsp-mode magit marginalia
+				markdown-mode mixed-pitch multiple-cursors mw-thesaurus neotree nyan-mode org
+				org-alert org-bullets org-capture org-capture-pop-frame org-fragtog
+				org-fragtop org-journal org-mode org-pomodoro org-rifle org-roam
+				org-super-agenda pandoc-mode pdf-tools tide use-package vterm yasnippet zotxt))
+ '(safe-local-variable-values '((TeX-command-extra-options . "-shell-escape")))
  '(tetris-x-colors
 	 [[229 192 123] [97 175 239] [209 154 102] [224 108 117] [152 195 121] [198 120 221]
 		[86 182 194]]))
