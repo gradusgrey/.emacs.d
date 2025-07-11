65ce936ebf3bef9870e7dca36326e8969738be6c
gradusgrey <ychen10.cxy@gmail.com>
2025-07-11 14:25:04 -0400

    clean up init

--- init.el
+++ init.el
@@ -2,24 +2,26 @@
 ;;; init.el
 (eval-when-compile
   (require 'package)
+  (require 'use-package)
 	(package-initialize)
-  (defvar use-package-verbose t)
-  (require 'use-package))
+  (defvar use-package-verbose t))
+
+(use-package package
+  :ensure nil
+  :custom
+  (package-vc-register-as-project nil)
+  (use-package-always-ensure t))
 
 ;;; Code:
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(setq use-package-always-ensure t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-revert-check-vc-info nil)
  '(calendar-today-invisible-hook nil)
  '(calendar-today-visible-hook nil)
- '(cdlatex-math-modify-prefix 96)
- '(cdlatex-math-symbol-prefix 64)
  '(custom-safe-themes
 	 '("0ad7f1c71fd0289f7549f0454c9b12005eddf9b76b7ead32a24d9cb1d16cbcbd"
 		 "1ed5c8b7478d505a358f578c00b58b430dde379b856fbcb60ed8d345fc95594e" default))
